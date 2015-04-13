f1c50105c832060f0c605691efdfc470d9c0f5d8
Nathan Smith <nathan@praisetopia.org>
2015-04-13 12:57:24 -0500

    Added main python script

--- aloha.py
+++ aloha.py
@@ -3,7 +3,8 @@ Data Comm Project
 Plain and Slotted ALOHA
 """
 
-def simulate_plain():
+def simulate_plain(tau, n):
+	"""tau is the window size, n is the number of transmissions"""
 
-def simulate_slotted():
-
+def simulate_slotted(tau, n):
+	"""tau is the window size, n is the number of transmissions"""
