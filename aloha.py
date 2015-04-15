275e7a00dcb301b2311c91c4b251b4abe08187c3
Nathan Smith <nathan@praisetopia.org>
2015-04-15 12:44:50 -0500

    working packet time generation function

--- aloha.py
+++ aloha.py
@@ -14,11 +14,14 @@ def simulate_slotted(tau, n):
 	"""tau is the window size, n is the number of transmissions"""
 	pass
 
-def get_packet_times(tau=1, n):
+def get_packet_times(n, tau=1):
 	step = 1.0/tau #For slotted, set window size to align times to
 	start_times = []
 
-	for i in range(n):
-		start_times.append(random.randrange(0, 1*step, tau*step)/step)
-
+	if tau != 1:
+		for i in range(n):
+			start_times.append(random.randrange(0, 1*step, tau*step)/step)
+	else:
+		for i in range(n):
+			start_times.append(random.random())
 	return start_times
