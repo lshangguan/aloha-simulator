6bed502b82985779b7a7b9dac5c45b38491b1a9c
Nathan Smith <nathan@praisetopia.org>
2015-04-15 13:04:37 -0500

    did the project, i think

--- aloha.py
+++ aloha.py
@@ -7,9 +7,21 @@ import random
 
 def simulate(tau, n, protocol='plain'):
 	"""tau is the window size, n is the number of transmissions, protocol should be plain/slotted (default plain)"""
-	times = get_packet_times(n, tau)
-
-	
+	#Get transmission times, sort them. 
+	if protocol == 'plain':
+		times = sorted(get_packet_times(n, 1))
+	else:
+		times = sorted(get_packet_times(n, tau))
+	collisions = 0
+	#Select either 2*tau for danger zone or tau for slotted
+	ws = 2*tau
+	if protocol == 'slotted':
+		ws = tau
+	for i in range(len(times)-1):
+		#Check for collisions
+		if times[i]+(ws) >= times[i+1]:
+			collisions += 1
+	return collisions
 
 def get_packet_times(n, tau=1):
 	step = 1.0/tau #For slotted, set window size to align times to
